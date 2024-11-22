256314aacd96c679ff49ba81f93e1d89f0fa2f37
iyn21 <iyinoluwa.oluleke@gmail.com>
2024-11-22 05:39:58 +0100

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
-
+import cors from 'cors';
 const app = express();
 const port = 3000;
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Dummy user roles data
 const userRoles = [
